165407bcc4254298b1cac9d2ee607aacf6e97786
Rizqi Nafila <rizqi.nafila24005@mhs.uingusdur.ac.id>
2026-09-22 09:14:24 +0700

    chore: restrukturisasi folder untuk Expo Router

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,32 @@
+// app/index.tsx
+import { useEffect, useState } from "react";
+import { View } from "react-native";
+// Catatan: gunakan ../components/ jika file berada di dalam folder app/
+// yang sejajar dengan folder components/
+import RiwayatList from "../../components/RiwayatList";
+import SearchBox from "../../components/SearchBox";
+import WeatherCard from "../../components/WeatherCard";
+
+export default function HalamanUtama() {
+  const [kotaAktif, setKotaAktif] = useState("Pekalongan");
+  const [riwayat, setRiwayat] = useState<string[]>(["Pekalongan"]);
+
+  // Tambahkan useEffect untuk mencatat perubahan kota aktif
+  useEffect(() => {
+    console.log("Kota aktif berubah menjadi:", kotaAktif);
+  }, [kotaAktif]);
+
+  function handleCari(kota: string) {
+    setKotaAktif(kota);
+    if (!riwayat.includes(kota)) {
+      setRiwayat([...riwayat, kota]);
+    }
+  }
+  return (
+    <View style={{ padding: 16, gap: 16 }}>
+      <SearchBox onCari={handleCari} />
+      <WeatherCard kota={kotaAktif} suhu={29} tingkatAQI="BAIK" />
+      <RiwayatList daftarKota={riwayat} />
+    </View>
+  );
+}
